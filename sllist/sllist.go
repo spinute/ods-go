718d28650820b035046fe2b9c7399479a1b6eb0d
spinute <spinutids@gmail.com>
2018-03-17 17:08:03 +0900

    add sllist and its tests

---
+++ sllist/sllist.go
@@ -0,0 +1,67 @@
+package sllist
+
+import (
+	"github.com/spinute/ods-go/utils"
+)
+
+type node struct {
+	x    utils.V
+	next *node
+}
+
+func nodeNew(x utils.V) *node {
+	return &node{
+		x: x,
+	}
+}
+
+type SLList struct {
+	n          int
+	head, tail *node
+}
+
+func New() *SLList {
+	return new(SLList)
+}
+
+func (l *SLList) Push(x utils.V) {
+	new_node := nodeNew(x)
+	new_node.next = l.head
+	l.head = new_node
+	if l.n == 0 {
+		l.tail = new_node
+	}
+	l.n++
+}
+
+func (l *SLList) Pop() utils.V {
+	if l.n == 0 {
+		return 0 // FIXME: handle error
+	}
+
+	ret := l.head.x
+	l.head = l.head.next
+
+	if l.n == 1 {
+		l.tail = nil
+	}
+	l.n--
+
+	return ret
+}
+
+func (l *SLList) Remove() utils.V {
+	return l.Pop()
+}
+
+func (l *SLList) Add(x utils.V) {
+	new_node := nodeNew(x)
+	if l.n == 0 {
+		l.head = new_node
+	} else {
+		l.tail.next = new_node
+	}
+
+	l.tail = new_node
+	l.n++
+}
---
+++ sllist/sllist_test.go
@@ -0,0 +1,64 @@
+package sllist
+
+import (
+	"github.com/spinute/ods-go/utils"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	if ret := New().n; ret != 0 {
+		t.Errorf("SLList.New().n = %v", ret)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	n := 10
+	sll := New()
+
+	for i := 0; i < n; i++ {
+		sll.Add(utils.V(i))
+		if sll.n != i+1 {
+			t.Errorf("sll.n = %v at %v th Add", sll.n, i+1)
+		}
+	}
+}
+
+func TestPush(t *testing.T) {
+	n := 10
+	sll := New()
+
+	for i := 0; i < n; i++ {
+		sll.Push(utils.V(i))
+		if sll.n != i+1 {
+			t.Errorf("sll.n = %v at %v th Push", sll.n, i+1)
+		}
+	}
+}
+
+func TestPushAndPop(t *testing.T) {
+	n := 10
+	sll := New()
+	for i := 0; i < n; i++ {
+		sll.Push(utils.V(i))
+	}
+
+	for i := 0; i < n; i++ {
+		if ret := sll.Pop(); ret != utils.V(n-1-i) {
+			t.Errorf("sll.Pop() = %v", i, ret)
+		}
+	}
+}
+
+func TestAddAndRemove(t *testing.T) {
+	n := 10
+	sll := New()
+	for i := 0; i < n; i++ {
+		sll.Add(utils.V(i))
+	}
+
+	for i := 0; i < n; i++ {
+		if ret := sll.Remove(); ret != utils.V(i) {
+			t.Errorf("sll.Remove() = %v", i, ret)
+		}
+	}
+}
