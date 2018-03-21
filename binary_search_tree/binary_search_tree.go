85bd1b950a2b825145978c46f22fd7c7cbb7a2e1
spinute <spinutids@gmail.com>
2018-03-22 00:26:34 +0900

    add binary_tree and its tests

---
+++ binary_search_tree/binary_search_tree.go
@@ -0,0 +1,141 @@
+package binary_search_tree
+
+import "github.com/spinute/ods-go/utils"
+
+type node struct {
+	left, right, parent *node
+	x                   utils.V
+}
+
+type BinarySearchTree struct {
+	r *node
+	n int
+}
+
+func New() *BinarySearchTree {
+	return &BinarySearchTree{}
+}
+
+func (bst *BinarySearchTree) FindEQ(x utils.V) bool {
+	w := bst.r
+	for w != nil {
+		comp := utils.Compare(x, w.x)
+		if comp < 0 {
+			w = w.left
+		} else if comp > 0 {
+			w = w.right
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func (bst *BinarySearchTree) Find(x utils.V) *utils.V {
+	w := bst.r
+	var ret *node = nil
+	for w != nil {
+		comp := utils.Compare(x, w.x)
+		if comp < 0 {
+			ret = w
+			w = w.left
+		} else if comp > 0 {
+			w = w.right
+		} else {
+			return &w.x
+		}
+	}
+	if ret == nil {
+		return nil
+	}
+	return &ret.x
+}
+
+func (bst *BinarySearchTree) Add(x utils.V) bool {
+	var ret bool
+	lastNode := bst.findLast(x)
+	newNode := node{x: x}
+	if lastNode == nil {
+		bst.r = &newNode
+		ret = true
+	} else {
+		ret = addChild(lastNode, &newNode)
+	}
+	if ret == true {
+		bst.n++
+	}
+	return ret
+}
+func (bst *BinarySearchTree) findLast(x utils.V) *node {
+	w := bst.r
+	last := w
+	for w != nil {
+		comp := utils.Compare(x, w.x)
+		last = w
+		if comp < 0 {
+		} else if comp > 0 {
+			w = w.right
+		} else {
+			return w
+		}
+	}
+	return last
+}
+func addChild(p, c *node) bool {
+	comp := utils.Compare(c.x, p.x)
+	if comp < 0 {
+		p.left = c
+	} else if comp > 0 {
+		p.right = c
+	} else {
+		return false
+	}
+	c.parent = p
+	return true
+}
+
+func (bst *BinarySearchTree) Remove(x utils.V) bool {
+	node := bst.findLast(x)
+	if node != nil && utils.Compare(x, node.x) == 0 {
+		bst.removeNode(node)
+		return true
+	}
+	return false
+}
+func (bst *BinarySearchTree) removeNode(node *node) {
+	if node.left == nil || node.right == nil {
+		bst.splice(node)
+	} else {
+		minInRight := node.right
+		for minInRight.left != nil {
+			minInRight = minInRight.left
+		}
+		node.x = minInRight.x
+		bst.splice(minInRight)
+	}
+	bst.n--
+}
+func (bst *BinarySearchTree) splice(n *node) {
+	var c, p *node
+	if n.left != nil {
+		c = n.left
+	} else {
+		c = n.right
+	}
+
+	if bst.r == n {
+		bst.r = c
+		p = nil
+	} else {
+		p = n.parent
+		if n == p.left {
+			p.left = c
+		} else {
+			p.right = c
+		}
+	}
+
+	if c != nil {
+		c.parent = p
+	}
+}
---
+++ binary_search_tree/binary_search_tree_test.go
@@ -0,0 +1,105 @@
+package binary_search_tree
+
+import (
+	"github.com/spinute/ods-go/utils"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	if ret := New().n; ret != 0 {
+		t.Errorf("BinarySearchTree.New().n = %v", ret)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	n := 10
+	bst := New()
+
+	for i := 0; i < n; i++ {
+		ret := bst.Add(utils.V(i))
+		if bst.n != i+1 {
+			t.Errorf("bst.n = %v at %v th Add", bst.n, i+1)
+		}
+		if ret == false {
+			t.Errorf("Add returned false unexpectedly")
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		ret := bst.Add(utils.V(i))
+		if bst.n != n {
+			t.Errorf("bst.n = %v", bst.n)
+		}
+		if ret == true {
+			t.Errorf("Add returned true unexpectedly")
+		}
+	}
+}
+
+func TestFindEQ(t *testing.T) {
+	n := 10
+	bst := New()
+
+	for i := 0; i < n; i++ {
+		ret := bst.FindEQ(utils.V(i))
+		if ret == true {
+			t.Errorf("Add returned non-nil unexpectedly, ret=%d", ret)
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		bst.Add(utils.V(i))
+		ret := bst.FindEQ(utils.V(i))
+		if ret == false {
+			t.Errorf("Add returned nil unexpectedly")
+		}
+	}
+
+	ret := bst.FindEQ(utils.V(n + 123))
+	if ret == true {
+		t.Errorf("Add returned nil unexpectedly")
+	}
+}
+
+func TestFind(t *testing.T) {
+	n := 10
+	bst := New()
+
+	for i := 0; i < n; i++ {
+		ret := bst.Find(utils.V(i))
+		if ret != nil {
+			t.Errorf("non-nil unexpectedly, ret=%d", ret)
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		bst.Add(utils.V(i))
+		ret := bst.Find(utils.V(i))
+		if ret == nil {
+			t.Errorf("Find returned nil unexpectedly")
+		}
+	}
+
+	ret := bst.Find(utils.V(n - 123))
+	if *ret != utils.V(0) {
+		t.Errorf("unexpectedly returned %v", *ret)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	x := utils.V(-12345)
+	bst := New()
+
+	if bst.Remove(x) == true {
+		t.Errorf("Add returned true unexpectedly")
+	}
+
+	bst.Add(x)
+	if bst.Remove(x) == false {
+		t.Errorf("Add returned false unexpectedly")
+	}
+
+	if bst.Find(x) != nil {
+		t.Errorf("Add returned non-nil unexpectedly")
+	}
+}
